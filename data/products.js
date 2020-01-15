8824e215a7a3a37dd9a51ecec90dbffcead65380
Abibibi <abeba.ngwe@gmail.com>
2020-01-15 21:22:44 +0100

    first commit: select and form elements already created, and select options read data

---
+++ data/products.js
@@ -0,0 +1,79 @@
+export default [
+    {
+        'Lait capillaire': [
+            
+            {
+                name: 'Leave-in Conditioning Repair Cream',
+                brand: 'Cantu',
+                picture: 'https://lecurlshop.com/2103-large_default/cantu-leave-in-conditioning-repair-cream.jpg',
+                category: 'Lait capillaire',
+                price: 7.95
+            },
+            {
+                name: 'Crème brulée',
+                brand: 'Curls',
+                picture: 'https://lecurlshop.com/2638-large_default/curls-creme-brulee.jpg',
+                category: 'Lait capillaire',
+                price: 14.90
+            },
+            {
+                name: 'Multi-use softening leave in conditioner',
+                brand: 'Melanin Haircare',
+                picture: 'https://cdn.shopify.com/s/files/1/0007/2256/7231/products/MUSLIC_Product_fea0a9ab-abef-4dc4-9eab-4a29ef991173.jpg?v=1575948415',
+                category: 'Lait capillaire',
+                price: 18.99
+            }
+
+        ]
+    },
+    {
+        'Après-shampoing': [
+            {
+                name: 'In control',
+                brand: 'Aunt Jackie\'s',
+                picture: 'https://www.colorfulblack.com/3836-large_default/aunt-jackie-s-curls-coils-in-control-conditioner-a-rincer.jpg',
+                category: 'Après-shampoing',
+                price: 9.90
+            },
+            {
+                name: 'Raw Shea Butter Restorative Conditioner',
+                brand: 'Shea Moisture',
+                picture: 'https://lecurlshop.com/693-large_default/apres-shampoing-ultra-demelant-knot-today.jpg',
+                category: 'Après-shampoing',
+                price: 14.90
+            },
+            {
+                name: 'Caribbean Coconut Milk Conditioner',
+                brand: 'Alikay Naturals',
+                picture: 'https://www.colorfulblack.com/3006-large_default/alikay-caribbean-coconut-milk-shampoo-236ml.jpg',
+                category: 'Après-shampoing',
+                price: 15.99
+            }
+        ]
+    },
+    {
+        'Shampoing': [
+            {
+                name: 'Pure Honey',
+                brand: 'Creme of Nature',
+                picture: 'https://www.colorfulblack.com/4469-large_default/creme-of-nature-pure-honey-shampooing-hydratant-355ml.jpg',
+                category: 'Shampoing',
+                price: 7.99
+            },
+            {
+                name: 'Hydrating Invigorating Shampoo',
+                brand: 'Jane Carter Solution',
+                picture: 'https://www.colorfulblack.com/1696-large_default/jane-carter-solution-hydrating-invigorating-shampoo.jpg',
+                category: 'Shampoing',
+                price: 11.90
+            },
+            {
+                name: 'Get Clarity',
+                brand: 'Entwine Couture',
+                picture: 'https://www.colorfulblack.com/4076-large_default/entwine-couture-shampooing-detoxifiant-get-clarity.jpg',
+                category: 'Shampoing',
+                price: 16.99
+            }
+        ]
+    }
+]
