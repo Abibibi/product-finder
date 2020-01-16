f291acd0f94015e450feb76ca9eb8dd156779f31
Abibibi <abeba.ngwe@gmail.com>
2020-01-16 01:06:06 +0100

    created Select and Form elements. Modularizing code for better readability

---
+++ js/Select/index.js
@@ -0,0 +1,29 @@
+import { App } from '../App.js';
+import products from '../../data/products.js';
+
+export const Select = () => {
+    // creating Select element
+    App.select = document.createElement('select');
+    App.select.classList.add('input-container-select');
+    App.select.title = 'Veuillez sélectionner un type de produit'
+
+    // creating Select 1st option (containing text to inform user about options type)
+    App.firstOption = document.createElement('option');
+    App.firstOption.textContent = 'Type de produit';
+
+    App.select.appendChild(App.firstOption);
+    
+    // creating following options values, which are read from data
+    products.map((category) => {
+        App.option = document.createElement('option');
+
+        const categoryName = Object.keys(category);
+        const lowerCategoryName = categoryName.toString().toLowerCase();
+
+        App.option.value = lowerCategoryName;
+        App.option.textContent = categoryName;
+        App.select.appendChild(App.option);
+    });
+    
+    App.inputContainer.appendChild(App.select);
+}
