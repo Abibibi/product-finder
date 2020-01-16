89a7c715ad082240755181b712e341a8da424bbb
Abibibi <abeba.ngwe@gmail.com>
2020-01-16 20:38:58 +0100

    all app logic in one file for now

--- js/ProductList/index.js
+++
@@ -1,8 +0,0 @@
-import App from '../App.js';
-
-export const productListDiv = document.createElement('div');
-
-export const ProductList = () => {
-    productListDiv.classList.add('products');
-    App.inputContainer.appendChild(productListDiv);
-};
